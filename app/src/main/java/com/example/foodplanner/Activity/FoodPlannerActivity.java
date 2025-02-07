06b7fa90248c453627e73aa646d2a72c97b7a9b7
MohammedHussien10 <mohammedhussien10101010@gmail.com>
2025-02-07 17:41:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/foodplanner/Activity/FoodPlannerActivity.java
@@ -0,0 +1,27 @@
+package com.example.foodplanner.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodplanner.Fragments.Loading_Home_Screen_Fragment;
+import com.example.foodplanner.R;
+
+public class FoodPlannerActivity extends AppCompatActivity {
+    Loading_Home_Screen_Fragment homeScreenFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.food_planner_main);
+        homeScreenFragment = new Loading_Home_Screen_Fragment();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, homeScreenFragment).commit();
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/foodplanner/Fragments/Login_Fragment.java
@@ -0,0 +1,46 @@
+package com.example.foodplanner.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foodplanner.R;
+
+
+public class Login_Fragment extends Fragment {
+
+
+    public Login_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
